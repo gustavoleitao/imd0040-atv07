edd47a5df729202a607680c57d06bf9a47b2c8f1
gustavoleitao <gustavo.leitao@imd.ufrn.br>
2022-01-20 20:15:20 -0300

    first commit

---
+++ src/main/java/br/ufrn/imd/FabricaElegibilidade.java
@@ -0,0 +1,17 @@
+package br.ufrn.imd;
+
+/**
+ * Fábrica responsável por construir instância válida da interface de Elegibilidade.
+ * @see Elegibilidade
+ */
+public class FabricaElegibilidade {
+
+    /**
+     * Cria instância da interface de Elegibilidade.
+     * @return instância coma estratégia ser utilizada
+     */
+    public static Elegibilidade create(){
+        throw new UnsupportedOperationException("Operação ainda não suportada");
+    }
+
+}
